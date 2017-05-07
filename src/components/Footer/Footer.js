8b9c1d640473ed0c857d1908b30d60f6f93acffe
Vincent Lo <vincentsylo@gmail.com>
2017-05-07 20:13:00 +1000

    Remove social from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -56,7 +56,6 @@ export default class Footer extends Component {
             <li><Link to="/shop">Shop</Link></li>
             <li><Link to="/menu">Menu</Link></li>
             <li><Link to="/about">About</Link></li>
-            <li><Link to="/social">Social</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
           <ul className={styles.list}>
