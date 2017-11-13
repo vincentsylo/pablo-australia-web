67027b348358524719ff810fa1f86c1b25cd1ce7
Vincent Lo <vincentsylo@gmail.com>
2017-11-13 23:29:08 +1100

    Feedback

--- src/views/Shops/Shops.js
+++ src/views/Shops/Shops.js
@@ -7,7 +7,7 @@ export default () => {
   const embedMap = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.471477912147!2d151.20799880307655
     !3d-33.87750932137191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12ae3cb0d09cd3%3A0xd23ac7561694422f
     !2s605+George+St%2C+Sydney+NSW+2000!5e0!3m2!1sen!2sau!4v1493446098120`;
-  const branchName = 'PABLO Sydney Goulburn Street';
+  const branchName = 'PABLO Sydney - Goulburn Street';
   const address = '605 George St (store located on Goulburn St), Sydney NSW';
   const telephone = '(02) 9267 2945';
   const hours = '11:00 AM - 10:00 PM';
